0f35badfc5330c5866d331f94376da7b00be8ae6
Damian Barczyński <daan.net@wp.eu>
2016-01-01 20:09:23 +0100

    Clean up.

--- source/SHA1.hpp
+++ source/SHA1.hpp
@@ -1,16 +1,7 @@
 /*
-	Copyright (c) 2015, Damian Barczynski <daan.net@wp.eu>
-
-	Permission to use, copy, modify, and/or distribute this software for any purpose
-	with or without fee is hereby granted, provided that the above copyright notice
-	and this permission notice appear in all copies.
-
-	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
-	TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN
-	NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
-	DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
-	IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
-	CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+    Copyright (c) 2015, Damian Barczynski <daan.net@wp.eu>
+    Following tool is licensed under the terms and conditions of the MIT license.
+    For more information see https://opensource.org/licenses/MIT.
 */
 #ifndef __SHA1_HPP__
 #define __SHA1_HPP__
@@ -20,48 +11,48 @@
 
 class SHA1
 {
-	typedef unsigned int uint;
-	typedef unsigned char byte;
-	typedef const std::string cstr;
+    typedef unsigned int uint;
+    typedef unsigned char byte;
+    typedef std::string string;
 
-	std::vector<std::vector<byte>> blocks;
+    std::vector<std::vector<byte>> blocks;
 
 public:
-	/*
-		\brief Convert message into const char* and transfer it to second ctor.
-		\param message_ string which will be hashed
-	*/
-	SHA1(cstr& message_);
+    /*
+        \brief Convert message into const char* and transfer it to second ctor.
+        \param message_ string which will be hashed
+    */
+    SHA1(const string& message_);
 
-	/*
-		\brief Prepare data block and cut it for 512-bit length chunks.
-		\param data_ pointer to data which are located in memory
-		\param size_ size of given data in bytes
-	*/
-	SHA1(const char* data_, uint size_);
+    /*
+        \brief Prepare data block and cut it for 512-bit length chunks.
+        \param data_ pointer to data which are located in memory
+        \param size_ size of given data in bytes
+    */
+    SHA1(const char* data_, uint size_);
 
-	/*
-		\brief Generate hash values from a single 512-bit block.
-		\param block_ pointer to first byte of 512-bit block
-		\param hash_ hash table with current hash values
-	*/
-	void transform(byte* block_, std::array<uint, 5>& hash_);
+    /*
+        \brief Generate hash values from a single 512-bit block.
+        \param block_ pointer to first byte of 512-bit block
+        \param hash_ hash table with current hash values
+    */
+    void transform(byte* block_, std::array<uint, 5>& hash_);
 
-	/*
-		\brief Transform all of blocks which are created by ctor.
-		\return hash value converted to string
-	*/
-	cstr generate();
+    /*
+        \brief Transform all of blocks which are created by ctor.
+        \return hash value converted to string
+    */
+    const string generate();
 
-	/*
-		\brief Perform left circular shift.
-		\param value_ 32 bits which will be shifted
-		\param shift_ number of cycles
-	*/
-	inline uint rotl(const uint& value_, const uint& shift_)
-	{
-		return ((value_ << shift_) | (value_ >> (32 - shift_)));
-	}
+    /*
+        \brief Perform left circular shift.
+        \param value_ 32 bits which will be shifted
+        \param shift_ number of cycles
+    */
+    uint rotl(const uint& value_, const uint& shift_)
+    {
+        return ((value_ << shift_) | (value_ >> (32 - shift_)));
+    }
 };
 
 #endif // __SHA1_HPP__
--- tests/run.cpp
+++ tests/run.cpp
@@ -7,26 +7,27 @@ using namespace std;
 
 void checkout(const string& text_, const string& output_)
 {
-	static int id_ = 0;
-	cout << "[Test " << setw(2) << ++id_ << "] status : " <<
-		(SHA1(text_).generate() == output_ ? "passed" : "failed") << endl;
+    static int id_ = 0;
+    cout << "[Test " << setw(2) << ++id_ << "] status : " <<
+        (SHA1(text_).generate() == output_ ? "passed" : "failed") << endl;
 }
 
 int main()
 {
-	map<string, string> tests;
+    map<string, string> tests;
 
-	tests.insert(make_pair("",		   "da39a3ee5e6b4b0d3255bfef95601890afd80709"));
-	tests.insert(make_pair("abc",	   "a9993e364706816aba3e25717850c26c9cd0d89d"));
-	tests.insert(make_pair("coarsely", "5fc6ddfb7ce1e7eed490f49bc1bdc68a951c3976"));
-	tests.insert(make_pair("flaggier", "a722cdbd38f915a87d424e10d8cb4b787bd2f08d"));
-	tests.insert(make_pair("morganic", "eebbebe01bdc3828e5cc33e5705a14d392adcbaa"));
-	tests.insert(make_pair("kankakee", "c6e57e838b2a7e3f20d2c48349e7716a683911f9"));
-	tests.insert(make_pair("crosslet", "04fcd808841718df3ff51c7d097900fb1d2281fa"));
-	tests.insert(make_pair("basilisk", "4bde28c14f8ae37923d08be77447927d186a33f4"));
-	tests.insert(make_pair("jeweller", "770db9155a39b00af1ecc75ee8eb5f72937059b2"));
-	tests.insert(make_pair("hawaiian", "b992de40bd2138b56212f208b1af60b329615780"));
+    tests.insert(make_pair("",         "da39a3ee5e6b4b0d3255bfef95601890afd80709"));
+    tests.insert(make_pair("abc",	   "a9993e364706816aba3e25717850c26c9cd0d89d"));
+    tests.insert(make_pair("coarsely", "5fc6ddfb7ce1e7eed490f49bc1bdc68a951c3976"));
+    tests.insert(make_pair("flaggier", "a722cdbd38f915a87d424e10d8cb4b787bd2f08d"));
+    tests.insert(make_pair("morganic", "eebbebe01bdc3828e5cc33e5705a14d392adcbaa"));
+    tests.insert(make_pair("kankakee", "c6e57e838b2a7e3f20d2c48349e7716a683911f9"));
+    tests.insert(make_pair("crosslet", "04fcd808841718df3ff51c7d097900fb1d2281fa"));
+    tests.insert(make_pair("basilisk", "4bde28c14f8ae37923d08be77447927d186a33f4"));
+    tests.insert(make_pair("jeweller", "770db9155a39b00af1ecc75ee8eb5f72937059b2"));
+    tests.insert(make_pair("hawaiian", "b992de40bd2138b56212f208b1af60b329615780"));
 
-	for (auto& test : tests)
-		checkout(test.first, test.second);
+    for (auto& test : tests) {
+        checkout(test.first, test.second);
+    }
 }
